d6b8397d4827cf67953e3e6b0aad4ea8483894c8
Marcus Vos <88241447+Marcus8009@users.noreply.github.com>
2024-11-18 08:53:06 +0000

    1

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "PUT"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
-@app.route("/main", methods=["GET", "PUT"])
+@app.route("/main", methods=["GET", "POST"])
 def main():
-    name = request.form.get("q")
-    return render_template("main.html")
-
+    name = request.form.get("q")  # Ensure 'q' matches the name attribute in your form
+    return render_template("main.html", name=name)
 
 if __name__ == "__main__":
     app.run(port=1111)
